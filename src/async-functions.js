3efd3b6b9ff407d8e3bb0a52a525a0b6b7045231
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-19 18:16:28 +0100

    done

--- src/async-functions.js
+++ src/async-functions.js
@@ -23,27 +23,25 @@ const addSomePromises = (somePromise) => {
 module.exports = { giveItBackLater, promiseToGiveItBackLater, addSomePromises };
 
 
-//////////////////////////////////////////////////////
-//       FUNCTIONS BEFORE I CLEANED UP CODE         //
-//////////////////////////////////////////////////////
-//                                                  //
-// function giveItBackLater(value, callback) {      //
-//     setTimeout (function() {                     //
-//         callback(value)                          //
-//     }, 100)                                      //
-// };                                               //
-//                                                  //
-// function promiseToGiveItBackLater(value) {       //
-//     return new Promise((resolve, reject) => {    //
-//         giveItBackLater(value)                   //
-//         resolve(value)                           //
-//         reject('Whoopsie!')                      //
-//     })                                           //
-// };                                               //
-//                                                  //
-// function addSomePromises(somePromise) {          //
-//     return somePromise                           //
-//         .then(string => string.repeat(2))        //
-//         .catch(string => string.repeat(3))       //
-// };                                               //
-//////////////////////////////////////////////////////
+
+// FUNCTIONS BEFORE I CLEANED UP CODE
+//                                                  
+// function giveItBackLater(value, callback) {      
+//     setTimeout (function() {                     
+//         callback(value)                          
+//     }, 100)                                      
+// };                                               
+//                                                  
+// function promiseToGiveItBackLater(value) {       
+//     return new Promise((resolve, reject) => {    
+//         giveItBackLater(value)                   
+//         resolve(value)                           
+//         reject('Whoopsie!')                      
+//     })                                           
+// };                                               
+//                                                  
+// function addSomePromises(somePromise) {          
+//     return somePromise                           
+//         .then(string => string.repeat(2))        
+//         .catch(string => string.repeat(3))       
+// };                                               
--- src/transform.js
+++ src/transform.js
@@ -1,39 +1,5 @@
-    
-//     const ageRanges = {
-//         '20 and younger': [],
-//         '21-30': [],
-//         '31-40': [],
-//         '41-50': []
-//     };
-   
-//     for (let i = 0; i < peopleArray.length; i++) {
-//         if (peopleArray[i].age >= 41) {
-//             ageRanges['41-50'].push(peopleArray[i])
-//         } else if (peopleArray[i].age >= 31) {
-//             ageRanges['31-40'].push(peopleArray[i])
-//         } else if (peopleArray[i].age >= 21) {
-//             ageRanges['21-30'].push(peopleArray[i])
-//         } else if (peopleArray[i].age < 21) {
-//             ageRanges['20 and younger'].push(peopleArray[i])
-//         };
-//     };
-
-//     if (ageRanges['41-50'].length === 0) {
-//         delete ageRanges['41-50']
-//     } else if (ageRanges['31-40'].length === 0) {
-//         delete ageRanges['31-40']
-//     } else if (ageRanges['21-30'].length === 0) {
-//         delete ageRanges['21-30']
-//     } else if (ageRanges['20 and younger'].length === 0) {
-//         delete ageRanges['20 and younger']
-//     };
-
-//     return ageRanges
-// }
 
 const groupAdultsByAgeRange = peopleArray => {
-
-
     return peopleArray
         .filter(person => person.age >=18)
         .reduce((obj, person) => {
@@ -41,21 +7,52 @@ const groupAdultsByAgeRange = peopleArray => {
             ageGroup = '51 and older'
         } else if (person.age >= 41) {
             ageGroup = '41-50'
-            //  = obj['41-50'].push(person)
         } else if (person.age >= 31) {
             ageGroup = '31-40'
-            //  = obj['31-40'].push(person)
         } else if (person.age >= 21) {
             ageGroup = '21-30'
-            //  = obj['21-30'].push(person)
-        } else {
+        } else if (person.age < 21) {
             ageGroup = '20 and younger'
-            //  = obj['20 and younger'].push(person)
         };
-        obj[ageGroup] = person;
+        obj[ageGroup] = [person]
+        obj[ageGroup] = [...obj[ageGroup], person]
         return obj
     }, {});
 };
 
 module.exports = { groupAdultsByAgeRange };
 
+
+// PLANNING OF FUNCTIONS BEFORE RE-WRITING
+//                                                                       
+// const ageRanges = {
+//     '20 and younger': [],
+//     '21-30': [],
+//     '31-40': [],
+//     '41-50': []};
+//
+// for (let i = 0; i < peopleArray.length; i++) {
+//     if (person.age >= 51) {
+//         ageRanges['51 and older'].push(peopleArray[i])
+//     } else if(peopleArray[i].age >= 41) {
+//         ageRanges['41-50'].push(peopleArray[i])
+//     } else if (peopleArray[i].age >= 31) {
+//         ageRanges['31-40'].push(peopleArray[i])
+//     } else if (peopleArray[i].age >= 21) {
+//         ageRanges['21-30'].push(peopleArray[i])
+//     } else if (peopleArray[i].age < 21 && peopleArray[i].age > 18) {
+//         ageRanges['20 and younger'].push(peopleArray[i])
+//     };};
+//
+// if (ageRanges['51 and older'].length === 0) {
+//     delete ageRanges['51 and older']
+// } else if (ageRanges['41-50'].length === 0) {
+//     delete ageRanges['41-50']
+// } else if (ageRanges['31-40'].length === 0) {
+//     delete ageRanges['31-40']
+// } else if (ageRanges['21-30'].length === 0) {
+//     delete ageRanges['21-30']
+// } else if (ageRanges['20 and younger'].length === 0) {
+//     delete ageRanges['20 and younger']
+// };
+// return ageRanges
